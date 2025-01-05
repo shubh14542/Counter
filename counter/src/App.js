f7da2cfd0ac5955bdffbb530df3aadffa2c9a285
Shubham <shubhampande2468@gmail.com>
2025-01-05 19:08:04 +0530

    this is project

--- counter/src/App.js
+++ counter/src/App.js
@@ -1,10 +1,16 @@
-
+import {useState} from 'react'
 import './App.css';
 
 function App() {
+  const [value,setValue] = useState(0)
   return (
     <div className="App">
           <h2>Counter App</h2>
+          <p>
+            {value}
+          </p>
+          <button onClick={()=>{setValue(value + 1)}} >Increase</button>
+          <button onClick={()=>{setValue(value - 1)}} >Decrease</button>
     </div>
   );
 }
